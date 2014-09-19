e8eebe46c54d66ab753e79424c76a30147a82005
Gabriel Jägenstedt <gabriel.jagenstedt@meht.org>
2014-09-19 13:46:43 +0200

    Modified Problem 8

--- 008/python/problem8.py
+++ 008/python/problem8.py
@@ -3,18 +3,17 @@
 from functools import reduce
 from operator import mul
 
+f = open("numbers")
+number = f.read()
+f.close()
+
 def maxsum(l):
   mx = 0
   for i in range(len(l)-1):
     mx = max(mx, reduce(mul, [int(x) for x in l[i:i+13]]))
   return mx
-    
 
-f = open("numbers")
-number = f.read()
-f.close()
-
-l = [x for x in number.split("0") if len(x) >= 13]
+sections = [x for x in number.split("0") if len(x) >= 13]
 mx = max([maxsum(x) for x in l])
 
 print(mx)
